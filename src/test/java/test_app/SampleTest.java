a818bd67ef13dc671b2dbd6c301a574caf8da06f
christbella <308667244@qq.com>
2020-06-07 10:46:41 +0800

    第一次测试

---
+++ src/test/java/test_app/SampleTest.java
@@ -0,0 +1,48 @@
+package test_app;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SampleTest {
+
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("appPackage", "com.xueqiu.android");
+        desiredCapabilities.setCapability("appActivity", ".view.WelcomeActivityAlias");
+        desiredCapabilities.setCapability("noReset", "true");
+        desiredCapabilities.setCapability("dontStopAppOnReset", "true");
+        desiredCapabilities.setCapability("adbExecTimeout", "50000");
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void sampleTest() {
+        MobileElement el4 = (MobileElement) driver.findElementById("com.xueqiu.android:id/home_search");
+        el4.click();
+        MobileElement el5 = (MobileElement) driver.findElementById("com.xueqiu.android:id/search_input_text");
+        el5.sendKeys("阿里巴巴");
+        MobileElement el6 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.TextView[1]");
+        el6.click();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
